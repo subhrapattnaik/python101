98364d1b8c5fbb0940dcc65f96b8c83097d31bf2
Subhra Pattnaik <50340351+subhrapattnaik@users.noreply.github.com>
2023-05-24 14:36:04 +0530

    Create turtle_parametricfunction.py

---
+++ turtle_parametricfunction.py
@@ -0,0 +1,26 @@
+#More Graphing with Turtle
+
+#Let's try to make some more beautiful graphs!
+
+#You can create more complex graphs using parametric functions. 
+
+#Even if you haven't learned about parametric functions yet in math class, experiment with the code given and see what happens!
+
+
+import turtle, math 
+
+pen = turtle.Turtle()
+pen.penup()
+pen.speed(0)
+pen.color("yellow")
+for i in range(0,628) :
+    t = i/50.0
+
+    # draw a flower
+    k = 6 
+    x = 100*math.cos(k*t)*math.cos(t)
+    y = 100*math.cos(k*t)*math.sin(t)
+        
+    if(not math.isnan(y)) :
+        pen.goto(x, y)
+        pen.pendown()
